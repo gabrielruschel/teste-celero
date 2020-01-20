5d5c2aa43ec78a413f22fdaa9a52fdca718024b6
Gabriel Ruschel <grc15@inf.ufpr.br>
2020-01-20 02:23:18 -0300

    Add Event model

--- src/olympics/models.py
+++ src/olympics/models.py
@@ -11,3 +11,16 @@ class Athlete(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+class Event(models.Model):
+    event_id = models.AutoField(primary_key=True)
+    athlete = models.ForeignKey(Athlete, on_delete=models.CASCADE)
+    team = models.CharField(max_length=35)
+    noc = models.CharField(max_length=4)
+    games = models.CharField(max_length=25)
+    year = models.IntegerField()
+    season = models.CharField(max_length=25)
+    city = models.CharField(max_length=20)
+    sport = models.CharField(max_length=35)
+    event = models.CharField(max_length=80)
+    medal = models.CharField(max_length=10)
