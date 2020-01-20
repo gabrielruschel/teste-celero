233e5ff29e9800b0f4a0f1f0873d9355bc2cb84d
Gabriel Ruschel <grc15@inf.ufpr.br>
2020-01-20 01:09:47 -0300

    Add Athlete model

--- src/olympics/models.py
+++ src/olympics/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Athlete(models.Model):
+    athlete_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=60)
+    sex = models.CharField(max_length=1)
+    age = models.CharField(max_length=4)
+    height = models.CharField(max_length=4)
+    weight = models.CharField(max_length=4)
+
+    def __str__(self):
+        return str(self.name)
