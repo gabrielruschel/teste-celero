58c59a373350da950b29ac6d972145f2d3fc816e
Gabriel Ruschel <grc15@inf.ufpr.br>
2020-01-20 14:50:35 -0300

    Event api ListCreate

--- src/olympics/api/urls.py
+++ src/olympics/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AthleteRudView,AthleteCreateView,EventRudView
+from .views import AthleteRudView,AthleteListCreateView,EventRudView,EventListCreateView
 
 urlpatterns = [
     path('athlete/<int:pk>/', AthleteRudView.as_view(), name='athlete-rud'),
-    path('athlete/', AthleteCreateView.as_view(), name='athlete-create'),
-    path('event/<int:pk>/', EventRudView.as_view(), name='event-rud')
+    path('athlete/', AthleteListCreateView.as_view(), name='athlete-list-create'),
+    path('event/<int:pk>/', EventRudView.as_view(), name='event-rud'),
+    path('event/', EventListCreateView.as_view(), name='event-list-create'),
 ]
--- src/olympics/api/views.py
+++ src/olympics/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from olympics.models import Athlete,Event
 from .serializers import AthleteSerializer,EventSerializer
 
-class AthleteCreateView(generics.ListCreateAPIView):
+class AthleteListCreateView(generics.ListCreateAPIView):
 
     lookup_field = 'pk'
     serializer_class = AthleteSerializer
@@ -31,3 +31,15 @@ class EventRudView(generics.RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         return Event.objects.all()
+
+class EventListCreateView(generics.ListCreateAPIView):
+
+    lookup_field = 'pk'
+    serializer_class = EventSerializer
+    # queryset = Athlete.objects.all()
+
+    def get_queryset(self):
+        return Event.objects.all()
+
+    def perform_create(self,serializer):
+        serializer.save()
