13d82936d15c1e65ca2107a757559d6f16457e3e
Gabriel Ruschel <grc15@inf.ufpr.br>
2020-01-20 02:34:42 -0300

    Alternative solution for the foreign key field

--- src/olympics/admin.py
+++ src/olympics/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from .models import Athlete,Event
 # Register your models here.
-admin.site.register(Athlete)
+
+class AthleteAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+admin.site.register(Athlete, AthleteAdmin)
 
 class EventAdmin(admin.ModelAdmin):
-    raw_id_fields = ('athlete',)
+    autocomplete_fields = ['athlete',]
 
 admin.site.register(Event,EventAdmin)
