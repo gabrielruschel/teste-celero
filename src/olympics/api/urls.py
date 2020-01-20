38fb1ed1283a69cda79f5737bb36374cced7f743
Gabriel Ruschel <grc15@inf.ufpr.br>
2020-01-20 11:01:34 -0300

    Athlete create view

--- src/olympics/api/urls.py
+++ src/olympics/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AthleteRudView
+from .views import AthleteRudView,AthleteCreateView
 
 urlpatterns = [
     path('athlete/<int:pk>/', AthleteRudView.as_view(), name='athlete-rud'),
+    path('athlete/', AthleteCreateView.as_view(), name='athlete-create'),
 ]
--- src/olympics/api/views.py
+++ src/olympics/api/views.py
@@ -2,6 +2,19 @@ from rest_framework import generics
 from olympics.models import Athlete
 from .serializers import AthleteSerializer
 
+class AthleteCreateView(generics.CreateAPIView):
+
+    lookup_field = 'pk'
+    serializer_class = AthleteSerializer
+    # queryset = Athlete.objects.all()
+
+    def get_queryset(self):
+        return Athlete.objects.all()
+
+    def perform_create(self,serializer):
+        serializer.save()
+
+
 class AthleteRudView(generics.RetrieveUpdateDestroyAPIView):
 
     lookup_field = 'pk'
