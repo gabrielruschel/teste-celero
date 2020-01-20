902c7953aa53b8ac83da49e0a52c48c064db6029
Gabriel Ruschel <grc15@inf.ufpr.br>
2020-01-20 02:25:10 -0300

    Change foreign key admin field to raw to avoid overload

--- src/olympics/admin.py
+++ src/olympics/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Athlete
+from .models import Athlete,Event
 # Register your models here.
 admin.site.register(Athlete)
+
+class EventAdmin(admin.ModelAdmin):
+    raw_id_fields = ('athlete',)
+
+admin.site.register(Event,EventAdmin)
