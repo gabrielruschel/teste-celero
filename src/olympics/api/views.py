e666ccb5fe4e56f5a1d90f45c69e5c8a5b9c8d89
Gabriel Ruschel <grc15@inf.ufpr.br>
2020-01-21 01:51:49 -0300

    Basic filters for Athlete

--- src/olympics/api/views.py
+++ src/olympics/api/views.py
@@ -9,7 +9,23 @@ class AthleteListCreateView(generics.ListCreateAPIView):
     # queryset = Athlete.objects.all()
 
     def get_queryset(self):
-        return Athlete.objects.all()
+        queryset = Athlete.objects.all()
+        
+        #Filter by name
+        query_param = self.request.query_params.get('name', None)
+        if (query_param is not None):
+            queryset = queryset.filter(name__icontains=query_param)
+
+        #Filter by sex
+        query_param = self.request.query_params.get('sex', None)
+        if (query_param is not None):
+            queryset = queryset.filter(sex__exact=query_param)
+
+        #Filter by age
+        query_param = self.request.query_params.get('age', None)
+        if (query_param is not None):
+            queryset = queryset.filter(age__exact=query_param)
+        return queryset
 
     def perform_create(self,serializer):
         serializer.save()
